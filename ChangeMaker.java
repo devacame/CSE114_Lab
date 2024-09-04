1badd4234c9176440021b5e7b04f4e5192813574
DEVACA <154665994+devacame@users.noreply.github.com>
2024-09-04 11:54:54 -0400

    Lab 01 + Lab03 for ChangeMaker

---
+++ ChangeMaker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ChangeMaker {
+    public static void main(String[] args) {
+        double amount = 0.0;
+        int inCents, rem, dollars, qs, ds, ns, ps;
+        Scanner input = new Scanner(System.in);
+        while (amount < 0) {
+            System.out.print("Input Amount: $");
+            amount = input.nextDouble();
+        }
+        input.close();
+        inCents = (int) (amount * 100);
+        dollars = inCents / 100;
+        rem = inCents % 100;
+        qs = rem / 25;
+        rem = rem % 25;
+        ds = rem / 10;
+        rem = rem % 10;
+        ns = rem / 5;
+        rem = rem % 5;
+        ps = rem;
+        System.out.println(dollars + " dollars, " + qs + " quarters, " + ds + " dimes, " + ns + " nickels, and " + ps
+                + " pennies.");
+    }
+}
---
+++ ComputeArea.java
@@ -0,0 +1,8 @@
+public class ComputeArea {
+    public static void main(String[] args) {
+        double radius, area;
+        radius = 20;
+        area = radius * radius * 3.14159;
+        System.out.println("The area for a cicle of radius " + radius + " is " + area);
+    }
+}
