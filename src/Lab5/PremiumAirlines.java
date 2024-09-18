4448a742639ae00a06369aef0739ee00c8f26e7c
DEVACA <154665994+devacame@users.noreply.github.com>
2024-09-17 22:13:42 -0400

    Refactor PremiumAirlines.java for improved membership input validation and points calculation

--- src/Lab5/PremiumAirlines.java
+++ src/Lab5/PremiumAirlines.java
@@ -7,9 +7,9 @@ public class PremiumAirlines {
         String membership = "";
         int price = -1, points = -1;
         Scanner input = new Scanner(System.in);
-        while (membership == "") {
+        while (!membership.matches("((p|P)latinum|(d|D)iamond|(r|R)egular)")) {
             System.out.print("Please enter membership (Diamond/Platinum/Regular): ");
-            membership = input.next();
+            membership = input.next().strip();
         }
         while (price < 0) {
             System.out.print("Please enter ticket price: ");
@@ -21,12 +21,12 @@ public class PremiumAirlines {
         }
         input.close();
 
-        if (!membership.equals("Regular") && price >= 5000) points += 35;
-        else if (membership.equals("Diamond") && price >= 2000 && points > 300) points += 30;
-        else if (membership.equals("Platinum") && price >= 2000) points += 20;
-        else if (membership.equals("Diamond") && price >= 500 && points >= 100) points += 10;
-        else if (membership.equals("Regular") && price >= 5000) points += 5;
-        else if (membership.equals("Diamond") && points >= 25) points += 2;
+        if (!membership.equalsIgnoreCase("Regular") && price >= 5000) points += 35;
+        else if (membership.equalsIgnoreCase("Diamond") && price >= 2000 && points > 300) points += 30;
+        else if (membership.equalsIgnoreCase("Platinum") && price >= 2000) points += 20;
+        else if (membership.equalsIgnoreCase("Diamond") && price >= 500 && points >= 100) points += 10;
+        else if (membership.equalsIgnoreCase("Regular") && price >= 5000) points += 5;
+        else if (membership.equalsIgnoreCase("Diamond") && points >= 25) points += 2;
 
         System.out.println("Points: " + points);
     }
