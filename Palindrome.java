368d3e223963c604d8f693fc213492c0744b94f1
DEVACA <154665994+devacame@users.noreply.github.com>
2024-10-12 13:07:56 -0400

    using a separate method per instructions

--- Palindrome.java
+++ Palindrome.java
@@ -3,19 +3,20 @@
 import java.util.Scanner;
 
 public class Palindrome {
+    public static boolean isPalindrome(String str) {
+        String ipClean = str.replaceAll("\\s+", "").toLowerCase();
+        for (int i=0;i<ipClean.length()/2;i++) {
+            if (ipClean.charAt(i) != ipClean.charAt(ipClean.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the input string: ");
         String ip = input.nextLine();
         input.close();
-        String ipClean = ip.replaceAll("\\s+", "").toLowerCase();
-        boolean isPalindrome = true;
-        for (int i=0;i<ipClean.length()/2;i++) {
-            if (ipClean.charAt(i) != ipClean.charAt(ipClean.length()-1-i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input string " + ip + " is " + (isPalindrome ? "" : "NOT ") + "a palindrome.");
+        System.out.println("Input string " + ip + " is " + (isPalindrome(ip) ? "" : "NOT ") + "a palindrome.");
     }
 }
